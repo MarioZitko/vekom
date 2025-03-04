15c4148ab5e1921bd4e4beeae582b8955c8f9e8a
MarioZitko <mariozitkovic2@gmail.com>
2025-03-05 00:35:04 +0100

    Deploy fixes

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://vekom.com', // Change this to your actual site URL
+  generateRobotsTxt: true, // Generates robots.txt file
+};
--- next-sitemap.config.ts
+++
@@ -1,8 +0,0 @@
-import { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
-  siteUrl: 'https://yourwebsite.com', // Change this to your actual site URL
-  generateRobotsTxt: true, // Generates robots.txt file
-};
-
-export default config;
